e6af620faff70f7ea0124addd427cd3358f11594
Joenhle <820863776@qq.com>
2024-04-29 01:18:12 +0800

    多个任务及时fast-fail

--- go/context/cancel2_test.go
+++ go/context/cancel2_test.go
@@ -5,31 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
-	"sync"
 	"testing"
 	"time"
 )
 
-type FastFailErrGroup struct {
-	once   sync.Once
-	ctx    context.Context
-	cancel context.CancelCauseFunc
-}
-
-func (g *FastFailErrGroup) Go(f func(ctx context.Context) error) {
-	g.once.Do(func() {
-		g.ctx, g.cancel =
-	})
-	go func(ctx context.Context, cancel context.CancelCauseFunc, f func(ctx2 context.Context) error) {
-		if err := f(ctx); err != nil {
-			cancel(err)
-		}
-	}(ctx, cancel, f)
-}
-
 func TestDoTask(t *testing.T) {
+	// 能做到多个任务并发执行，如果有一个失败则ctx传播及时阻断，且外部能用ctx.Done进行fast-fail，并利用context.Cause(ctx)返回被cancel的原因，且所有任务都完成也能感知。
 	ctx, cancel := context.WithCancelCause(context.Background())
-
+	suc_chan := make(chan struct{}, 2)
 	f1 := func(ctx2 context.Context) error {
 		for {
 			select {
@@ -42,7 +25,6 @@ func TestDoTask(t *testing.T) {
 			}
 		}
 	}
-
 	f2 := func(ctx2 context.Context) error {
 		for {
 			select {
@@ -51,10 +33,36 @@ func TestDoTask(t *testing.T) {
 				return nil
 			default:
 				time.Sleep(1 * time.Second)
-				if rand.Intn(10) > 5 {
+				if rand.Intn(10) > 8 {
 					return errors.New("f2 error")
 				}
 			}
 		}
 	}
+	for _, f := range []func(ctx2 context.Context) error{f1, f2} {
+		f := f
+		go func() {
+			if err := f(ctx); err != nil {
+				cancel(err)
+			} else {
+				suc_chan <- struct{}{}
+			}
+		}()
+	}
+	i := 0
+LOOP:
+	for {
+		select {
+		case <-suc_chan:
+			i += 1
+			if i == 2 {
+				fmt.Println("all success!")
+				break LOOP
+			}
+		case <-ctx.Done():
+			fmt.Println(context.Cause(ctx))
+			break LOOP
+		}
+	}
+	time.Sleep(10 * time.Second)
 }
